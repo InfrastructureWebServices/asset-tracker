6d2f7223542adbf75298e0bdd46ce9eee85e2736
Brendan Horne <brendan@infrastructurewebservices.com>
2024-07-02 13:38:10 +1000

    basic picking slip complete, update model to support picking slips

--- generate_picking_slip.py
+++ generate_picking_slip.py
@@ -1,19 +1,26 @@
 from openpyxl import load_workbook
-from openpyxl.styles import Border, Side
+from openpyxl.styles import Border, Side, Alignment
 from os import path
+import re
+reference_pattern = re.compile(r'([A-Z]+)([0-9]+)')
+
 directory = path.dirname(__file__)
+
 wb = load_workbook(path.join(directory, 'data', 'template.xlsx'))
 
-border = Border(
-    left=Side(border_style='medium', color='00000000'),
-    right=Side(border_style='medium', color='00000000'),
-    bottom=Side(border_style='medium', color='00000000'),
-    top=Side(border_style='medium', color='00000000')
+
+all_borders = Border(
+    left=Side(border_style='thin', color='00000000'),
+    right=Side(border_style='thin', color='00000000'),
+    bottom=Side(border_style='thin', color='00000000'),
+    top=Side(border_style='thin', color='00000000')
     )
+
+center_wrapped = Alignment(horizontal='center', vertical='bottom', wrap_text=True)
 
 import json
 with open('picking_slip.json', 'r') as f:
-    data = json.loads(f.read())
+    pick_data = json.loads(f.read())
 
 ws = wb['Form']
 
@@ -22,12 +29,57 @@ defined_names = wb.defined_names
 asset_table = ws.tables['asset_table']
 
 insert_row_id = int(defined_names['insert_row_after'].value)
-ws.insert_rows(insert_row_id, amount=2)
 
-asset_table.ref = asset_table.ref[:-1] + str(insert_row_id-1+2)
+row_count = 0
+for i in range(0, len(pick_data)-1):
+    ws.insert_rows(insert_row_id, amount=1)
+    row_count += 1
 
-data_range = asset_table.split(':')
-data_range[0] = data_range[0][:-1] + (int(data_range[0][]))
+
+def shift_row_ref(ref, shift):
+    ref = reference_pattern.findall(ref)
+    ref = ref[0][0] + str(int(ref[0][1]) + shift)
+    return ref
+
+[from_ref, to_ref] = asset_table.ref.split(':')
+asset_table.ref = from_ref + ":" + shift_row_ref(to_ref, row_count)
+[from_ref, to_ref] = asset_table.ref.split(':')
+data_range = ws[shift_row_ref(from_ref, 1) + ":" + to_ref]
+
+def set_cell(cell, value):
+    if value != None:
+        cell.value = value
+    cell.border = all_borders
+    cell.alignment = center_wrapped
+    return cell
+
+domain = "http://localhost:5000/"
+
+def set_linked_cell(cell, value):
+    cell = set_cell(cell, value)
+    cell.hyperlink = "%s/assets/%s" % (domain, value)
+    cell.style = 'Hyperlink'
+    return cell
+
+def set_pick_item(row, item):
+    set_linked_cell(row[0], item.get('id'))
+    set_cell(row[1], item.get('description'))
+    set_cell(row[2], item.get('manufacturer'))
+    set_cell(row[3], item.get('part_number'))
+    set_cell(row[4], item.get('serial_number'))
+    set_cell(row[5], 1)
+
+row_count = 0
+for item in pick_data:
+    set_pick_item(data_range[row_count], item)    
+    row_count += 1
+
+for row in ws[asset_table.ref]:
+    for cell in row:
+        cell.border = all_borders
+
+# shift or remove defined name references?
+
 
 wb.save(path.join(directory, 'output', 'output.xlsx'))
 
--- model/model.py
+++ model/model.py
@@ -1,9 +1,7 @@
 from os import environ
 from flask_login import UserMixin
-from sqlalchemy import Boolean, Integer, String, DateTime, UUID
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, Integer, String, DateTime, UUID, create_engine, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy.sql import func
 from sqlalchemy.schema import Identity
 from flask_sqlalchemy import SQLAlchemy
@@ -21,6 +19,13 @@ class User(UserMixin, db.Model):
     first_name: Mapped[String] = mapped_column(String)
     last_name: Mapped[String] = mapped_column(String)
 
+    pick_lists_requested = relationship('Pick_List', foreign_keys="Pick_List.requested_by_id", back_populates='requested_by')
+    pick_lists_received = relationship('Pick_List',  foreign_keys="Pick_List.received_by_id", back_populates='received_by')
+    pick_lists_delivered = relationship('Pick_List', foreign_keys="Pick_List.delivered_by_id", back_populates='delivered_by')
+    comments = relationship('Comment', back_populates='user')
+    change_logs = relationship('Change_Log', back_populates='user')
+    scans = relationship('Scan', back_populates='user')
+
 class Verification_Session(db.Model):
     __tablename__ = "verification_sessions"
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
@@ -33,17 +38,22 @@ class Verification_Session(db.Model):
 class Asset(db.Model):
     __tablename__ = "assets"
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name: Mapped[String] = mapped_column(String, nullable=True)
+    description: Mapped[String] = mapped_column(String, nullable=True)
     manufacturer: Mapped[String] = mapped_column(String, nullable=True)
     part_number: Mapped[String] = mapped_column(String, nullable=True)
     serial_number: Mapped[String] = mapped_column(String, nullable=True)
     location: Mapped[String] = mapped_column(String, nullable=True) # link to foreign table
     owner: Mapped[String] = mapped_column(String, nullable=True)
 
+    comments = relationship('Comment', back_populates='asset')
+    change_logs = relationship('Change_Log', back_populates='asset')
+    scans = relationship('Scan', back_populates='asset')
+    pick_items = relationship('Pick_Item', back_populates='asset')
+
     def serialize(self):
         return { 
             "id": self.id,
-            "name": self.name,
+            "description": self.description,
             "manufacturer": self.manufacturer, 
             "part_number": self.part_number,
             "serial_number": self.serial_number,
@@ -51,26 +61,60 @@ class Asset(db.Model):
             "owner": self.owner
         }
 
-class Comments(db.Model):
+class Comment(db.Model):
     __tablename__ = "asset_comments"
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
-    asset_id: Mapped[UUID] = mapped_column(UUID)
+    asset_id: Mapped[UUID] = mapped_column(UUID, ForeignKey('assets.id'))
     value: Mapped[String] = mapped_column(String)
-    user_id: Mapped[Integer] = mapped_column(String)
+    user_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('users.id'))
     ts: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
 
-class Scans(db.Model):
+    asset = relationship('Asset', back_populates='comments')
+    user = relationship('User', back_populates='comments')
+
+class Scan(db.Model):
     __tablename__ = "asset_scans"
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
-    asset_id: Mapped[UUID] = mapped_column(UUID)
-    user_id: Mapped[Integer] = mapped_column(String)
+    asset_id: Mapped[UUID] = mapped_column(UUID, ForeignKey('assets.id'))
+    user_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('users.id'))
     ts: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+
+    asset = relationship('Asset', back_populates='scans')
+    user = relationship('User', back_populates='scans')
 
 class Change_Log(db.Model):
-    __tablename__ = "asset_change_log"
+    __tablename__ = "asset_change_logs"
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
-    asset_id: Mapped[UUID] = mapped_column(UUID)
+    asset_id: Mapped[UUID] = mapped_column(UUID, ForeignKey('assets.id'))
     value: Mapped[String] = mapped_column(String)
-    user_id: Mapped[Integer] = mapped_column(String)
+    user_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('users.id'))
     ts: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
 
+    asset = relationship('Asset', back_populates='change_logs')
+    user = relationship('User', back_populates='change_logs')
+
+class Pick_List(db.Model):
+    __tablename__ = "pick_lists"
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    requested_by_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    requested_by_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    delivered_by_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    delivered_by_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    received_by_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    received_by_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+
+    items = relationship('Pick_Item', back_populates='pick_list')
+    requested_by = relationship('User', foreign_keys="Pick_List.requested_by_id", back_populates='pick_lists_requested')
+    delivered_by = relationship('User', foreign_keys="Pick_List.delivered_by_id", back_populates='pick_lists_delivered')
+    received_by = relationship('User', foreign_keys="Pick_List.received_by_id", back_populates='pick_lists_received')
+
+class Pick_Item(db.Model):
+    __tablename__ = "pick_items"
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    pick_list_id: Mapped[int] = mapped_column(Integer, ForeignKey("pick_lists.id"), nullable=False)
+    asset_id: Mapped[UUID] = mapped_column(UUID, ForeignKey('assets.id'))
+    status: Mapped[String] = mapped_column(String, nullable=False) # waiting for picking, picked, waiting for delivery, delivered, waiting for receiving, received
+    ts: Mapped[DateTime] = mapped_column(DateTime, onupdate=func.now(), server_default=func.now())
+
+    pick_list = relationship('Pick_List', back_populates='items')
+    asset = relationship('Asset', back_populates='pick_items')
